1e4a5ce0df10f065adfcc652a04c7b73af5561a5
Ariano <arianowong@gmail.com>
2020-03-25 20:56:24 +0100

    Update

--- HRDelicates/HRDelicates/Program.cs
+++ HRDelicates/HRDelicates/Program.cs
@@ -9,40 +9,67 @@ namespace HRDelicates
 	{
 		static void Main(string[] args)
 		{
-			//string dish_file = File.ReadAllText(@"dishes.json");
+			string retry = "No";
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-			//dynamic dishes = JsonConvert.DeserializeObject(dish_file);
-			//foreach (var n in dishes)
-			//{
-			//	Console.WriteLine(n.name);
-
-			//}
-
-			// Startscreen
-			Console.WriteLine(FiggleFonts.Standard.Render("HRDelicates"));
-			Console.WriteLine("Welkom bij de HRDelicates Reservation App!");
-			Console.WriteLine("------------------------------------------\n");
-			Console.WriteLine("Typ 'r' om te reserveren");
-			Console.WriteLine("Typ 'm' om de menu te zien");
-			Console.WriteLine("Typ 'c' om de contactgegevens te zien");
-			Console.WriteLine("Druk op Enter als u een letter heeft ingetypt\n");
-			Console.Write("Typ in: ");
-
-			switch (Console.ReadLine())
+			do
 			{
-				case "r":
-					Console.WriteLine("U heeft gekozen voor r");
-					break;
-				case "m":
-					Console.WriteLine("U heeft gekozen voor m");
-					break;
-				case "c":
-					Console.WriteLine("U heeft gekozen voor c");
-					break;
-				default:
-					Console.WriteLine("Er is iets misgegaan. Probeer het opnieuw");
-					break;
-			}
+				// Startscreen
+				Console.Clear();
+				Console.WriteLine(FiggleFonts.Standard.Render("HRDelicates"));
+				Console.WriteLine("Welkom bij de HRDelicates Reservation App!");
+				Console.WriteLine("------------------------------------------\n");
+				Console.WriteLine("Typ 'r' om te reserveren");
+				Console.WriteLine("Typ 'm' om de menu te zien");
+				Console.WriteLine("Typ 'c' om de contactgegevens te zien");
+				Console.WriteLine("Typ 'q' om de applicatie te sluiten");
+				Console.WriteLine("Druk op Enter als u een letter heeft ingetypt\n");
+				Console.Write("Typ in: ");
+			
+				switch (Console.ReadLine())
+				{
+					case "r":
+						Console.WriteLine("U heeft gekozen voor r");
+						break;
+					case "m":
+						Console.Clear();
+						string dish_file = File.ReadAllText(@"dishes.json");
+						dynamic dishes = JsonConvert.DeserializeObject(dish_file);
+
+						Console.WriteLine("\nGerechten");
+						Console.WriteLine("------------------------------------------");
+
+						foreach (var n in dishes)
+						{
+							Console.Write(n.name + " | ");
+							Console.Write("\u20AC" + n.price + "\n");
+						}
+
+						Console.WriteLine("\nDranken");
+						Console.WriteLine("------------------------------------------");
+
+						foreach (var n in dishes)
+						{
+							Console.Write(n.name + " | ");
+							Console.Write("\u20AC" + n.price + "\n");
+						}
+
+						Console.Write("\nDruk enter om terug naar het hoofdmenu te gaan");
+						retry = Console.ReadLine();
+
+						break;
+					case "c":
+						Console.WriteLine("U heeft gekozen voor c");
+						break;
+					case "q":
+						Environment.Exit(0);
+						break;
+					default:
+						Console.WriteLine("Er is iets misgegaan. Probeer het opnieuw");
+						retry = Console.ReadLine();
+						break;
+				}
+			} while (retry != "No");
 		}
 	}
 }
