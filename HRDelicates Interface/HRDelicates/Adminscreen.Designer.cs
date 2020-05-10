b9e65c973dbaf523cf04789bdb584836bfb28eab
Ariano <arianowong@gmail.com>
2020-05-10 15:01:39 +0200

    Admin table fix

--- HRDelicates Interface/HRDelicates/Adminscreen.Designer.cs
+++ HRDelicates Interface/HRDelicates/Adminscreen.Designer.cs
@@ -37,7 +37,6 @@
             this.capaciteitDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.statusDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.persoonDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.aPPDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dreserveringDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.timeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -69,7 +68,6 @@
             this.capaciteitDataGridViewTextBoxColumn,
             this.statusDataGridViewTextBoxColumn,
             this.persoonDataGridViewTextBoxColumn,
-            this.aPPDataGridViewTextBoxColumn,
             this.dreserveringDataGridViewTextBoxColumn,
             this.timeDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.tablesBindingSource;
@@ -84,14 +82,16 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 72;
             this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.dataGridView1.ShowCellToolTips = false;
             this.dataGridView1.ShowEditingIcon = false;
-            this.dataGridView1.Size = new System.Drawing.Size(890, 467);
+            this.dataGridView1.Size = new System.Drawing.Size(1632, 862);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -103,65 +103,70 @@
             // 
             this.nummerDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.nummerDataGridViewTextBoxColumn.DataPropertyName = "Nummer";
-            this.nummerDataGridViewTextBoxColumn.HeaderText = "Nummer";
+            this.nummerDataGridViewTextBoxColumn.HeaderText = "Tafelnummer";
+            this.nummerDataGridViewTextBoxColumn.MinimumWidth = 9;
             this.nummerDataGridViewTextBoxColumn.Name = "nummerDataGridViewTextBoxColumn";
             this.nummerDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nummerDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // capaciteitDataGridViewTextBoxColumn
             // 
             this.capaciteitDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.capaciteitDataGridViewTextBoxColumn.DataPropertyName = "Capaciteit";
             this.capaciteitDataGridViewTextBoxColumn.HeaderText = "Capaciteit";
+            this.capaciteitDataGridViewTextBoxColumn.MinimumWidth = 9;
             this.capaciteitDataGridViewTextBoxColumn.Name = "capaciteitDataGridViewTextBoxColumn";
             this.capaciteitDataGridViewTextBoxColumn.ReadOnly = true;
+            this.capaciteitDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // statusDataGridViewTextBoxColumn
             // 
             this.statusDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.statusDataGridViewTextBoxColumn.DataPropertyName = "Status";
             this.statusDataGridViewTextBoxColumn.HeaderText = "Status";
+            this.statusDataGridViewTextBoxColumn.MinimumWidth = 9;
             this.statusDataGridViewTextBoxColumn.Name = "statusDataGridViewTextBoxColumn";
             this.statusDataGridViewTextBoxColumn.ReadOnly = true;
+            this.statusDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // persoonDataGridViewTextBoxColumn
             // 
             this.persoonDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.persoonDataGridViewTextBoxColumn.DataPropertyName = "Persoon";
             this.persoonDataGridViewTextBoxColumn.HeaderText = "Persoon";
+            this.persoonDataGridViewTextBoxColumn.MinimumWidth = 9;
             this.persoonDataGridViewTextBoxColumn.Name = "persoonDataGridViewTextBoxColumn";
             this.persoonDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // aPPDataGridViewTextBoxColumn
-            // 
-            this.aPPDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.aPPDataGridViewTextBoxColumn.DataPropertyName = "A_P_P";
-            this.aPPDataGridViewTextBoxColumn.HeaderText = "A_P_P";
-            this.aPPDataGridViewTextBoxColumn.Name = "aPPDataGridViewTextBoxColumn";
-            this.aPPDataGridViewTextBoxColumn.ReadOnly = true;
+            this.persoonDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // dreserveringDataGridViewTextBoxColumn
             // 
             this.dreserveringDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.dreserveringDataGridViewTextBoxColumn.DataPropertyName = "D_reservering";
-            this.dreserveringDataGridViewTextBoxColumn.HeaderText = "D_reservering";
+            this.dreserveringDataGridViewTextBoxColumn.HeaderText = "Datum reservering";
+            this.dreserveringDataGridViewTextBoxColumn.MinimumWidth = 9;
             this.dreserveringDataGridViewTextBoxColumn.Name = "dreserveringDataGridViewTextBoxColumn";
             this.dreserveringDataGridViewTextBoxColumn.ReadOnly = true;
+            this.dreserveringDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // timeDataGridViewTextBoxColumn
             // 
             this.timeDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.timeDataGridViewTextBoxColumn.DataPropertyName = "Time";
-            this.timeDataGridViewTextBoxColumn.HeaderText = "Time";
+            this.timeDataGridViewTextBoxColumn.HeaderText = "Tijd";
+            this.timeDataGridViewTextBoxColumn.MinimumWidth = 9;
             this.timeDataGridViewTextBoxColumn.Name = "timeDataGridViewTextBoxColumn";
             this.timeDataGridViewTextBoxColumn.ReadOnly = true;
+            this.timeDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // Adminscreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Adminscreen";
-            this.Size = new System.Drawing.Size(890, 467);
+            this.Size = new System.Drawing.Size(1632, 862);
             this.Load += new System.EventHandler(this.Adminscreen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tablesBindingSource)).EndInit();
@@ -172,13 +177,12 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource tablesBindingSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn nummerDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn capaciteitDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn statusDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn persoonDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn aPPDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dreserveringDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn timeDataGridViewTextBoxColumn;
-        private System.Windows.Forms.BindingSource tablesBindingSource;
     }
 }
